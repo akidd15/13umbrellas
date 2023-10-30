103d66cf14e1e4c0bfa9994cde70ecf7c04ffd48
Allison Farris <allison.perry98@gmail.com>
2023-10-30 14:18:17 -0600

    added associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,13 +5,21 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'catagory_id',
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_name',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+foreignKey: 'product_id',
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: 'tag_name'
+});
 module.exports = {
   Product,
   Category,
